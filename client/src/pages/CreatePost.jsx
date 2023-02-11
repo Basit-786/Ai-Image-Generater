6751c9034b83755791f3dc36e85a0173e3acce47
Basit-0786 <arsajid090@gmail.com>
2023-02-11 10:09:21 +0500

    server link updated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,7 +24,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://ai-image-generater-ja8z.vercel.app/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
