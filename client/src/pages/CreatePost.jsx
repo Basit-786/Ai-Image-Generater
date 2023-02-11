14ccf2aaa3cec14cc910822ec1fc4ed0878a8e00
Basit-0786 <arsajid090@gmail.com>
2023-02-11 10:14:35 +0500

    link changed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -55,7 +55,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://ai-image-generater-ja8z.vercel.app/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
